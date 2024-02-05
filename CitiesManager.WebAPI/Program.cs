cc28560cea88fd0e3d37c79f08eb8c8f3283d561
ramanrana-infostride <raman.rana@infostride.com>
2024-02-05 20:01:24 +0530

    Fix

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -83,7 +83,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 
 app.UseHsts();
 app.UseHttpsRedirection();
