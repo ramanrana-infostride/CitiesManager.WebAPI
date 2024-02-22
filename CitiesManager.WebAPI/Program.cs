7af1ec128113b3c1b4222399cdf8bd1f4418a4ec
ramanrana-infostride <raman.rana@infostride.com>
2024-02-22 12:31:20 +0530

    Test

--- CitiesManager.WebAPI/Program.cs
+++ CitiesManager.WebAPI/Program.cs
@@ -16,7 +16,7 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the  container.
 
 builder.Services.AddControllers(options =>
 {
